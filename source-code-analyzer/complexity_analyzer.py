c5c118b67f2a46199c7391b2db67bab5b9af4af2
RyazanGrove <53538807+RyazanGrove@users.noreply.github.com>
2023-08-27 20:38:31 +0300

    add mi, hal and raw analysis methods
    maintainability index analysis
    Halstead volume analysis
    Raw metrics: ines of code and percentage of comments analysis

--- source-code-analyzer/complexity_analyzer.py
+++ source-code-analyzer/complexity_analyzer.py
@@ -10,12 +10,40 @@ print('Started analyzing dir: ' + target_dir)
 
 total_cc_count = 0
 total_cc_files = 0
+total_mi_count = 0
+total_mi_files = 0
+total_hal_count = 0
+total_hal_files = 0
+total_raw_loc_count = 0
+total_raw_comments_percentage_count = 0
+total_raw_files = 0
 
 def parse_cc_response(file_name, response):
     response = json.loads(response)
     if file_name in response:
         return (response[file_name][0]['complexity'], 1)
     return (0,0)
+
+def parse_mi_response(file_name, response):
+    response = json.loads(response)
+    if file_name in response:
+        return (response[file_name]['mi'], 1)
+    return (0,0)
+
+def parse_hal_response(file_name, response):
+    response = json.loads(response)
+    if file_name in response:
+        return (response[file_name]['total']['volume'], 1)
+    return (0,0)
+
+def parse_raw_response(file_name, response):
+    response = json.loads(response)
+    if file_name in response:
+        loc = response[file_name]['loc']
+        comments = response[file_name]['comments'] + response[file_name]['multi']
+        comments_percentage = comments / loc if loc != 0 else 0
+        return (loc, comments_percentage, 1)
+    return (0,0, 0)
 
 for root, subdirs, files in os.walk(target_dir):
     for file in files:
@@ -27,10 +55,36 @@ for root, subdirs, files in os.walk(target_dir):
             total_cc_count += res[0]
             total_cc_files += res[1]
 
+            execution_str = "radon mi -j " + file
+            output_str = subprocess.getoutput(execution_str)
+            res = parse_mi_response(file, output_str)
+            total_mi_count += res[0]
+            total_mi_files += res[1]
+
+            execution_str = "radon hal -j " + file
+            output_str = subprocess.getoutput(execution_str)
+            res = parse_hal_response(file, output_str)
+            total_hal_count += res[0]
+            total_hal_files += res[1]
+
+            execution_str = "radon raw -j " + file
+            output_str = subprocess.getoutput(execution_str)
+            res = parse_raw_response(file, output_str)
+            total_raw_loc_count += res[0]
+            total_raw_comments_percentage_count += res[1]
+            total_raw_files += res[2]
 
 print("Total cyclomatic complexity is: ",total_cc_count)
-print("Analyzed files for cyclomatic complexity",total_cc_files)
+print("Analyzed files for cyclomatic complexity: ",total_cc_files)
 
+print("Total maintainability index is: ",total_mi_count)
+print("Analyzed files for maintainability index: ",total_mi_files)
 
+print("Total Halstead volume is: ",total_hal_count)
+print("Analyzed files for Halstead volume: ",total_hal_files)
+
+print("Total raw loc is: ",total_raw_loc_count)
+print("Total raw comments percentage is: ",total_raw_comments_percentage_count)
+print("Analyzed files for raw metrics: ",total_raw_files)
 
     
