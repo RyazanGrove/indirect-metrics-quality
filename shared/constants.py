2df0e2ab013759836b4883f6f63ecae629c78909
RyazanGrove <53538807+RyazanGrove@users.noreply.github.com>
2023-10-17 17:38:35 +0300

    add service metadata in constants

--- shared/constants.py
+++ shared/constants.py
@@ -10,19 +10,21 @@ operational_metrics_names = ["Static tests", "Dynamic tests", "Integration tests
                              "End-to-end tests", "Test Pass Rate", "Build related",
                              "Pipeline Pass Rate", "Security scan", "Code quality scan"]
 
-operational_metrics_per_service = {
-    "S1": [operational_metrics_names[i] for i in [0,1,2,4,5,6,7]],
-    "S2": [operational_metrics_names[i] for i in [0,1,4,5,6,7,8]],
-    "S3": [operational_metrics_names[i] for i in [0,1,4,5,6,7]],
-    "S4": [operational_metrics_names[i] for i in [0,1,4,5,6,7]],
-    "S5": [operational_metrics_names[i] for i in [0,1,4,5,6,7]],
-    "S6": [operational_metrics_names[i] for i in [0,1,4,5,6,7,8]],
-    "S7": [operational_metrics_names[i] for i in [5,6]],
-    "S8": [operational_metrics_names[i] for i in [0,1,4,5,6,7]],
-    "S9": [operational_metrics_names[i] for i in [0,1,4,5,6,8]],
-    "S10": [operational_metrics_names[i] for i in [0,1,4,5,6,8]],
-    "S11": [operational_metrics_names[i] for i in [3,4,5,6]]
-}
-
 verification_start_date = '2023-09-13'
 verification_end_date = '2023-09-27'
+
+services_metadata = {
+    "operational": [
+        { "service": "S1", "sheet_name": "1 rt-orchestration-service", "metrics": [operational_metrics_names[i] for i in [0,1,2,4,5,6,7]]},
+        { "service": "S2", "sheet_name": "2 rt-image2plan-ui-lib", "metrics": [operational_metrics_names[i] for i in [0,1,4,5,6,7,8]]},
+        { "service": "S3", "sheet_name": "3 rt-auth-service", "metrics": [operational_metrics_names[i] for i in [0,1,4,5,6,7]]},
+        { "service": "S4", "sheet_name": "4 rt-dicom-comm-service", "metrics": [operational_metrics_names[i] for i in [0,1,4,5,6,7]]},
+        { "service": "S5", "sheet_name": "5 rt-image2plan-config-editor-s", "metrics": [operational_metrics_names[i] for i in [0,1,4,5,6,7]]},
+        { "service": "S6", "sheet_name": "6 rt-image2plan-infra-service", "metrics": [operational_metrics_names[i] for i in [0,1,4,5,6,7,8]]},
+        { "service": "S7", "sheet_name": "7 rt-opentext-etx-service", "metrics": [operational_metrics_names[i] for i in [5,6]]},
+        { "service": "S8", "sheet_name": "8 rt-remote-desktop-service", "metrics": [operational_metrics_names[i] for i in [0,1,4,5,6,7]]},
+        { "service": "S9", "sheet_name": "9 rt-bdd-ui-test-lib", "metrics": [operational_metrics_names[i] for i in [0,1,4,5,6,8]]},
+        { "service": "S10", "sheet_name": "10 rt-bdd-api-test-lib", "metrics": [operational_metrics_names[i] for i in [0,1,4,5,6,8]]},
+        { "service": "S11", "sheet_name": "11 rt-orchestrator-e2e-testing", "metrics": [operational_metrics_names[i] for i in [3,4,5,6]]},
+    ]
+}
