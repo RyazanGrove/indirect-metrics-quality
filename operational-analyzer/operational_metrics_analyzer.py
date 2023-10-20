776eb424b90683b36048e1c9052f302994ed6c84
RyazanGrove <53538807+RyazanGrove@users.noreply.github.com>
2023-10-20 14:16:27 +0300

    add pearson and spearman correlations

--- operational-analyzer/operational_metrics_analyzer.py
+++ operational-analyzer/operational_metrics_analyzer.py
@@ -1,12 +1,20 @@
-from shared.constants import verification_start_date, services_metadata
+from shared.constants import verification_start_date, services_metadata, operational_metrics_names
 from shared.defects_calculator import get_defect_dict
 
 import pandas as pd
+import numpy as np
 
 defect_dict = get_defect_dict()
 
-for current_service in services_metadata["operational"]:
+dimensions_array = np.zeros((9,11))
+correlation_pearson_df = pd.DataFrame(dimensions_array)
+correlation_spearman_df = correlation_pearson_df.copy()
+std_df = correlation_pearson_df.copy()
+service_names_list = []
+
+for service_index, current_service in enumerate(services_metadata["operational"]):
     service_name = current_service["service"]
+    service_names_list.append(service_name)
     columns_to_analyze = current_service["metrics"]
     sheet_name = current_service["sheet_name"]
 
@@ -25,9 +33,27 @@ for current_service in services_metadata["operational"]:
             if(row['Week'] in defect_dict["2023"]):
                 operational_metrics_data.loc[index, 'Danger'] = defect_dict["2023"][row['Week']]
 
+    #print the whole data table for ml model
+    operational_metrics_data.to_excel('..\\output\\operational_metrics\\operational_metrics_for_ml_' + service_name + '.xlsx')
+
     verification_period = operational_metrics_data[ operational_metrics_data['Date'] >=  verification_start_date].index
     operational_metrics_data.drop(verification_period , inplace=True)
 
-    print("Metrics for service : ", service_name)
+    print('\nNumber of items in the dataset for service {0}: {1}'.format(service_name, operational_metrics_data.shape[0]))
     for column in columns_to_analyze:
-        print('{0} correlation = {1}'.format(column, operational_metrics_data[column].corr(operational_metrics_data["Danger"])))
+        metric_index = operational_metrics_names.index(column)
+        correlation_pearson_df.iloc[metric_index, service_index] = operational_metrics_data[column].corr(operational_metrics_data["Danger"], method='pearson')
+        correlation_spearman_df.iloc[metric_index, service_index] = operational_metrics_data[column].corr(operational_metrics_data["Danger"], method='spearman')
+        std_df.iloc[metric_index, service_index] = operational_metrics_data[column].std()
+
+correlation_pearson_df.columns = service_names_list
+correlation_spearman_df.columns = service_names_list
+std_df.columns = service_names_list
+
+correlation_pearson_df.to_excel('..\\output\\operational_metrics\\operational_metrics_corr_pearson.xlsx', float_format="%.3f")
+correlation_spearman_df.to_excel('..\\output\\operational_metrics\\operational_metrics_corr_spearman.xlsx', float_format="%.3f")
+std_df.to_excel('..\\output\\operational_metrics\\operational_metrics_std.xlsx', float_format="%.3f")
+
+print("Pearson correlation", correlation_pearson_df)
+print("Spearman correlation", correlation_spearman_df)
+print("Std", std_df)
