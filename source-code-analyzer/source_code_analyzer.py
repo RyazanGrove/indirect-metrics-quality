0e4b820d8468e5ce066552115fae761c9f03237a
RyazanGrove <53538807+RyazanGrove@users.noreply.github.com>
2023-10-22 21:41:28 +0300

    add coefficient of variation to source code metrics

--- source-code-analyzer/source_code_analyzer.py
+++ source-code-analyzer/source_code_analyzer.py
@@ -17,9 +17,10 @@ dimensions_array = np.zeros((len(columns_to_analyze),6))
 correlation_pearson_df = pd.DataFrame(dimensions_array)
 correlation_spearman_df = correlation_pearson_df.copy()
 std_df = correlation_pearson_df.copy()
+coefficient_of_variation_df = correlation_pearson_df.copy()
 service_names_list = []
 
-for index_main, current_service in enumerate(services_metadata["source_code"]):
+for service_index, current_service in enumerate(services_metadata["source_code"]):
     service_name = current_service["service"]
     service_names_list.append(service_name)
     sheet_name = current_service["sheet_name"]
@@ -41,26 +42,30 @@ for index_main, current_service in enumerate(services_metadata["source_code"]):
                 source_code_data.loc[index, 'Danger'] = defect_dict["2023"][row['Week']]
 
     #print the whole data table for ml model
-    source_code_data.to_excel('..\\output\\source_code_for_ml_' + service_name + '.xlsx')
+    source_code_data.to_excel('..\\output\\source_code\\source_code_for_ml_' + service_name + '.xlsx')
 
     #analyze only the data before verification period
     verification_period = source_code_data[ source_code_data['Date'] >=  verification_start_date].index
     source_code_data.drop(verification_period , inplace=True)
 
     print('\nNumber of items in the dataset for service {0}: {1}'.format(service_name, source_code_data.shape[0]))
-    for index, column in enumerate(columns_to_analyze):
-        correlation_pearson_df.iloc[index, index_main] = source_code_data[column].corr(source_code_data["Danger"], method='pearson')
-        correlation_spearman_df.iloc[index, index_main] = source_code_data[column].corr(source_code_data["Danger"], method='spearman')
-        std_df.iloc[index, index_main] = source_code_data[column].std()
+    for metric_index, column in enumerate(columns_to_analyze):
+        correlation_pearson_df.iloc[metric_index, service_index] = source_code_data[column].corr(source_code_data["Danger"], method='pearson')
+        correlation_spearman_df.iloc[metric_index, service_index] = source_code_data[column].corr(source_code_data["Danger"], method='spearman')
+        std_df.iloc[metric_index, service_index] = source_code_data[column].std()
+        coefficient_of_variation_df.iloc[metric_index, service_index] = source_code_data[column].std() / source_code_data[column].mean()
 
 correlation_pearson_df.columns = service_names_list
 correlation_spearman_df.columns = service_names_list
 std_df.columns = service_names_list
+coefficient_of_variation_df.columns = service_names_list
 
-correlation_pearson_df.to_excel('..\\output\\source_code_corr_pearson.xlsx', float_format="%.3f")
-correlation_spearman_df.to_excel('..\\output\\source_code_corr_spearman.xlsx', float_format="%.3f")
-std_df.to_excel('..\\output\\source_code_std.xlsx', float_format="%.3f")
+correlation_pearson_df.to_excel('..\\output\\source_code\\source_code_corr_pearson.xlsx', float_format="%.3f")
+correlation_spearman_df.to_excel('..\\output\\source_code\\source_code_corr_spearman.xlsx', float_format="%.3f")
+std_df.to_excel('..\\output\\source_code\\source_code_std.xlsx', float_format="%.3f")
+coefficient_of_variation_df.to_excel('..\\output\\source_code\\source_code_cv.xlsx', float_format="%.3f")
 
 print("Pearson correlation", correlation_pearson_df)
 print("Spearman correlation", correlation_spearman_df)
 print("Std", std_df)
+print("Coefficient of variation", coefficient_of_variation_df)
