4382e29e1741100829412de46e19f18f9ee2c3d2
RyazanGrove <53538807+RyazanGrove@users.noreply.github.com>
2023-11-25 19:21:58 +0200

    add get_defect_for_sprint

--- shared/defects_calculator.py
+++ shared/defects_calculator.py
@@ -10,7 +10,7 @@ def get_defect_dict():
     print("Calculating total number of bugs with weight for every week")
 
     #read
-    bugs_sheet = pd.read_excel('..\\data\\Masters thesis draft 1.4.xlsx', sheet_name='Bugs sorted', index_col=0)
+    bugs_sheet = pd.read_excel('..\\data\\Masters thesis final.xlsx', sheet_name='Bugs sorted', index_col=0)
     #remove rows not related to defects
     real_bugs = bugs_sheet.loc[bugs_sheet['Danger'].notnull()]
     #remove rows with unclear danger
@@ -41,3 +41,34 @@ def get_defect_dict():
     print(result)
     
     return result
+
+def get_defect_for_sprint():
+    print("Calculating total number of bugs with weight for every sprint")
+
+    #read
+    sprints_sheet = pd.read_excel('..\\data\\Masters thesis final.xlsx', sheet_name='Sprints')
+
+    #read
+    bugs_sheet = pd.read_excel('..\\data\\Masters thesis final.xlsx', sheet_name='Bugs sorted', index_col=0)
+    #remove rows not related to defects
+    real_bugs = bugs_sheet.loc[bugs_sheet['Danger'].notnull()]
+    #remove rows with unclear danger
+    real_bugs["Danger_lvl"] = pd.to_numeric(real_bugs["Danger"], errors='coerce')
+    real_bugs = real_bugs.loc[real_bugs['Danger_lvl'].notnull()]
+
+    #create week and year columns
+    real_bugs["Week"] = real_bugs["Found"].dt.isocalendar().week
+    real_bugs["Year"] = real_bugs["Found"].dt.isocalendar().year
+
+    bugs_per_sprint = {}
+
+    for index, row in real_bugs.iterrows():
+        sprint = sprints_sheet.loc[sprints_sheet['Date'] == row['Found']]
+        if(not sprint.empty):
+            spr = sprint.iloc[0,1]
+            if (spr not in bugs_per_sprint):
+                bugs_per_sprint[spr] = row['Danger_lvl']
+            else:
+                bugs_per_sprint[spr] = bugs_per_sprint[spr] + row['Danger_lvl']
+
+    return bugs_per_sprint
