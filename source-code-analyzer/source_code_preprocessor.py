a698382991e6fc1b6e78c7f530153878fe4c230d
RyazanGrove <53538807+RyazanGrove@users.noreply.github.com>
2023-11-25 20:45:27 +0200

    add abbreviations for services in source code metrics

--- source-code-analyzer/source_code_preprocessor.py
+++ source-code-analyzer/source_code_preprocessor.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import numpy as np
 
-from shared.defects_calculator import get_defect_dict
-from shared.constants import verification_start_date, services_metadata
+from shared.defects_calculator import get_defect_dict, get_defect_dict_per_service
+from shared.constants import verification_start_date, vpr_start_date, services_metadata
 
 columns_to_analyze = [
 "Total Cyclomatic Complexity", "Average Cyclomatic Complexity per file", 
@@ -12,18 +12,19 @@ columns_to_analyze = [
 "Total percentage of comments (raw analysis)", "Average percentage of comments (raw analysis) per file"
 ]
 
-defect_dict = get_defect_dict()
 dimensions_array = np.zeros((len(columns_to_analyze),6))
 correlation_pearson_df = pd.DataFrame(dimensions_array)
 correlation_spearman_df = correlation_pearson_df.copy()
 std_df = correlation_pearson_df.copy()
 coefficient_of_variation_df = correlation_pearson_df.copy()
-service_names_list = []
+service_abr_list = []
 
 for service_index, current_service in enumerate(services_metadata["source_code"]):
-    service_name = current_service["service"]
-    service_names_list.append(service_name)
+    service_abr = current_service["service"]
+    service_abr_list.append(service_abr)
     sheet_name = current_service["sheet_name"]
+    service_name = current_service["service_name"]
+    defect_dict = get_defect_dict_per_service(service_name)
 
     source_code_data = pd.read_excel('..\\data\\Masters thesis final.xlsx', sheet_name=sheet_name)
 
@@ -42,23 +43,23 @@ for service_index, current_service in enumerate(services_metadata["source_code"]
                 source_code_data.loc[index, 'Danger'] = defect_dict["2023"][row['Week']]
 
     #print the whole data table for ml model
-    source_code_data.to_excel('..\\output\\source_code\\source_code_for_ml_' + service_name + '.xlsx')
+    source_code_data.to_excel('..output\\source_code\\source_code_for_ml' + service_abr + '.xlsx')
 
     #analyze only the data before verification period
     verification_period = source_code_data[ source_code_data['Date'] >=  verification_start_date].index
     source_code_data.drop(verification_period , inplace=True)
 
-    print('\nNumber of items in the dataset for service {0}: {1}'.format(service_name, source_code_data.shape[0]))
+    print('\nNumber of items in the dataset for service {0}: {1}'.format(service_abr, source_code_data.shape[0]))
     for metric_index, column in enumerate(columns_to_analyze):
         correlation_pearson_df.iloc[metric_index, service_index] = source_code_data[column].corr(source_code_data["Danger"], method='pearson')
         correlation_spearman_df.iloc[metric_index, service_index] = source_code_data[column].corr(source_code_data["Danger"], method='spearman')
         std_df.iloc[metric_index, service_index] = source_code_data[column].std()
         coefficient_of_variation_df.iloc[metric_index, service_index] = source_code_data[column].std() / source_code_data[column].mean()
 
-correlation_pearson_df.columns = service_names_list
-correlation_spearman_df.columns = service_names_list
-std_df.columns = service_names_list
-coefficient_of_variation_df.columns = service_names_list
+correlation_pearson_df.columns = service_abr_list
+correlation_spearman_df.columns = service_abr_list
+std_df.columns = service_abr_list
+coefficient_of_variation_df.columns = service_abr_list
 
 correlation_pearson_df.to_excel('..\\output\\source_code\\source_code_corr_pearson.xlsx', float_format="%.3f")
 correlation_spearman_df.to_excel('..\\output\\source_code\\source_code_corr_spearman.xlsx', float_format="%.3f")
