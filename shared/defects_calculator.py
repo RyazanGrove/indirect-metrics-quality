a8ec269d44372d563e149bf4c1673cc08ab9754e
RyazanGrove <53538807+RyazanGrove@users.noreply.github.com>
2023-11-25 19:26:24 +0200

    add get_defect_dict_per_service function

--- shared/defects_calculator.py
+++ shared/defects_calculator.py
@@ -38,7 +38,6 @@ def get_defect_dict():
                 danger_lvls_2023_dict[row['Week']] = danger_lvls_2023_dict[row['Week']] + row['Danger_lvl']
 
     result = {"2022": danger_lvls_2022_dict, "2023": danger_lvls_2023_dict}
-    print(result)
     
     return result
 
@@ -72,3 +71,40 @@ def get_defect_for_sprint():
                 bugs_per_sprint[spr] = bugs_per_sprint[spr] + row['Danger_lvl']
 
     return bugs_per_sprint
+
+def get_defect_dict_per_service(service_name: str = 'rt-orchestration-service'):
+    print("Calculating total number of bugs with weight for every week per " + service_name + " service")
+
+    #read
+    bugs_sheet = pd.read_excel('..\\data\\Masters thesis final.xlsx', sheet_name='Bugs sorted', index_col=0)
+    #remove rows not related to defects
+    real_bugs = bugs_sheet.loc[bugs_sheet['Danger'].notnull()]
+    #remove rows with unclear danger
+    real_bugs["Danger_lvl"] = pd.to_numeric(real_bugs["Danger"], errors='coerce')
+    real_bugs = real_bugs.loc[real_bugs['Danger_lvl'].notnull()]
+    
+    real_bugs = real_bugs.loc[real_bugs['Related service (at least fix)'].str.contains(service_name)]
+
+    real_bugs["Week"] = real_bugs["Found"].dt.isocalendar().week
+    real_bugs["Year"] = real_bugs["Found"].dt.isocalendar().year
+
+    danger_lvls_2022_dict = {}
+    danger_lvls_2023_dict = {}
+
+    for index, row in real_bugs.iterrows():
+        if (row['Year'] == 2022):
+            if (row['Week'] not in danger_lvls_2022_dict):
+                danger_lvls_2022_dict[row['Week']] = row['Danger_lvl']
+            else:
+                danger_lvls_2022_dict[row['Week']] = danger_lvls_2022_dict[row['Week']] + row['Danger_lvl']
+        
+        if (row['Year'] == 2023):
+            if (row['Week'] not in danger_lvls_2023_dict):
+                danger_lvls_2023_dict[row['Week']] = row['Danger_lvl']
+            else:
+                danger_lvls_2023_dict[row['Week']] = danger_lvls_2023_dict[row['Week']] + row['Danger_lvl']
+
+    result = {"2022": danger_lvls_2022_dict, "2023": danger_lvls_2023_dict}
+    print(result)
+    
+    return result
